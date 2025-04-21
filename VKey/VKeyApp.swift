49e1d137edd4874fbff2d671367c2348fdd37d7d
Radomyr Sidenko <radik-sidehnko@ukr.net>
2025-04-21 14:36:07 +0300

    Initial Commit

---
+++ VKey/VKeyApp.swift
@@ -0,0 +1,17 @@
+//
+//  VKeyApp.swift
+//  VKey
+//
+//  Created by Radomyr Sidenko on 21.04.2025.
+//
+
+import SwiftUI
+
+@main
+struct VKeyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
